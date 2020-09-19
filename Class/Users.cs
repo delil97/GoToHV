dd8a02bac9b55a573fc30ee9e10c6b9554224767
delil97 <58556097+delil97@users.noreply.github.com>
2020-09-19 11:00:58 +0200

    Create Users.cs

---
+++ Class/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoToHV.Class
+{
+    class Users
+    {
+        public string userName { get; set; }
+
+        public string password { get; set; }
+
+
+    }
+}
